87dcfcf155f96db99f5393df157f205c8c87b414
Adrian <adrian.carstea@gmail.com>
2017-11-05 17:46:30 +0200

    checking stack heap memory allocation

---
+++ ex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint8_t a = 0;
+
+int main(){
+	uint8_t *ptr = (uint8_t *) malloc(2);
+	uint32_t b = 23;
+	*ptr = 5;
+	*(ptr+1) = 6;
+
+	for(int i=0;i<2;i++){
+		printf("%p\t%p\t%d\n",ptr,&(*(ptr+i)),ptr[i]);
+	}
+
+	free(ptr);
+	return 0;
+}
