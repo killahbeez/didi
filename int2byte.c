ff46f5e69cfd78c56a922335985d6d18ae15d6e7
Adrian <adrian.carstea@gmail.com>
2017-11-09 02:56:21 +0200

    change int2byte

--- int2byte.c
+++ int2byte.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
 
-union test {
-	uint32_t a;
+static union test {
+	uint32_t whole;
 	struct {
 		char c0;
 		char c1;
 		char c2;
 		char c3;
 	} bytes;
-};
+} int2byte;
 
 int main(int argc, char *argv[]){
-	union test int2byte;
-	int2byte.a = 0x11223344;
-	printf("%x\n",int2byte.a);
+	int2byte.whole = 0x11223344;
+	printf("sizeof enum: %d bytes\n",sizeof(int2byte));
+	printf("int2byte address: %p\n",&int2byte);
+	printf("c0 address: %p\n",&(int2byte.bytes.c0));
+	printf("c1 address: %p\n",&(int2byte.bytes.c1));
+	printf("c2 address: %p\n",&(int2byte.bytes.c2));
+	printf("c3 address: %p\n",&(int2byte.bytes.c3));
+	printf("%x\n",int2byte.whole);
 	printf("c0: %x\tc1: %x\tc2: %x\tc2: %x\n",
 			int2byte.bytes.c0,
 			int2byte.bytes.c1,
