7df302ff6ab809fec251ceaed02dfa2fdcd7233a
Adrian <adrian.carstea@gmail.com>
2017-11-11 02:11:45 +0200

    proving nul terminated string and stack pointers

--- ex.c
+++ ex.c
@@ -6,6 +6,17 @@
 char* didi();
 
 int main(){
+	char q[] = "didi do dexter";
+	char w[] = "cacat pansat ";
+
+	uint16_t string_len = strlen(w);
+	uint32_t padding_bytes = (q-w-string_len);
+	// change NUL character and all the padding bytes (end of string) with a non printable one
+	for(int i=0;i<padding_bytes;i++){
+		*(w+string_len+i) = 1;		
+	}
+	printf("%s\n",w);
+
 	char* didilea = NULL;
 	didilea = (char *) didi();
 	*(didilea + 1) = 'U';
