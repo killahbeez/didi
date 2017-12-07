52ea55ec3b5f535584b77587af98e74dd7fa89b6
Adrian <adrian.carstea@gmail.com>
2017-12-07 15:20:25 +0200

    nanosleep example

--- ex2.c
+++ ex2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 int didi[][2][3] = {
 		{
@@ -17,11 +18,32 @@ int didi[][2][3] = {
 		}
 };
 
+void swap(int *,int *);
+
 int main(){
 	int cnt =0;
 	for(int i=0;i<3*2*3;i++){
 		printf("%d\n",*((int*)didi + cnt++));
 	}
 
+	int a = -345;
+	int b = 786;
+	printf("a: %d\tb: %d\n", a, b);
+	swap(&a,&b);
+	printf("a: %d\tb: %d\n", a, b);
+	struct timespec tim;
+   	tim.tv_sec = 0;
+    tim.tv_nsec = 500000000;
+
+	for(int i=0;i<10;i++){
+		nanosleep(&tim, NULL);
+		printf("\t%d\n",i);
+	}
 	return 0;
+}
+
+void swap(int *a,int *b){
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
 }
