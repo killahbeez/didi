c1b86ed19ff453e7defb6b99346825a02702b3e1
Adrian <adrian.carstea@gmail.com>
2018-01-27 18:32:11 +0200

    reverse string modified

--- reverse_string.c
+++ reverse_string.c
@@ -3,16 +3,17 @@
 #include <stdint.h>
 #include <string.h>
 
-char *reverse_str(char *str,int size);
+char *reverse_str(char *str);
 int main(int argc, char *argv[]){
 	char *didi = "abcdefghijklmnoprstuvwxyz";
-	char *rev_didi = reverse_str(didi,strlen(didi));
+	char *rev_didi = reverse_str(didi);
 	printf("Length: %d\nString: %s\nLength: %d\nReverse string: %s\n",strlen(didi),didi,strlen(rev_didi),rev_didi);
 	free(rev_didi);
 	return 0;
 }
 
-char *reverse_str(char *str, int size){
+char *reverse_str(char *str){
+	uint16_t size = strlen(str);
 	char *buffer = (char *) malloc(size+1);
 	int i=0;
 	while(size > 0){
