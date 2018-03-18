256c4c677c397bead6dabae16b1cd88dac7d5932
Adrian <adrian.carstea@gmail.com>
2018-03-18 23:52:38 +0200

    modify testsort

--- testsort.c
+++ testsort.c
@@ -2,35 +2,35 @@
 #include <string.h>
 #include <stdio.h>
 
-static int
-compare (const void *a, const void *b)
-{
-  return strcmp (*(char **) a, *(char **) b);
+static int compare (const void *a, const void *b) {
+	return strcmp (*(char **) a, *(char **) b);
 }
 
-int
-main (void)
-{
-  char bufs[500][20];
-  char *lines[500];
-  size_t lens[500];
-  size_t i, j;
+int main (void) {
+	char bufs[500][20];
+	char *lines[500];
+	size_t lens[500];
+	size_t i, j;
 
-  srandom (1);
+	srandom (1);
 
-  for (i = 0; i < 500; ++i)
-    {
-      lens[i] = random() % 19;
-      lines[i] = bufs[i];
-      for (j = 0; j < lens[i]; ++j)
-	lines[i][j] = random() % 26 + 'a';
-      lines[i][j] = '\0';
-    }
+	for (i = 0; i < 500; ++i)
+	{
+		lens[i] = random() % 19;
+		while(!lens[i]){
+			lens[i] = random() % 19;
+		}
+		lines[i] = bufs[i];
+		for (j = 0; j < lens[i]; ++j)
+			lines[i][j] = random() % 26 + 'a';
+		lines[i][j] = '\0';
+		//printf("%d\t%d: %s\n",i,lens[i],lines[i]);
+	}
 
-  qsort (lines, 500, sizeof (char *), compare);
+	qsort (lines, 500, sizeof (char *), compare);
 
-  for (i = 0; i < 500 && lines[i] != NULL; ++i)
-    puts (lines[i]);
+	for (i = 0; i < 500 && lines[i] != NULL; ++i)
+		puts (lines[i]);
 
-  return 0;
+	return 0;
 }
