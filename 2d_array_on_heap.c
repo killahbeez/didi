9591697773dba509e14147c07f8205eabf8258a8
Adrian <adrian.carstea@gmail.com>
2018-07-17 18:45:24 +0300

    free 2d heap array

--- 2d_array_on_heap.c
+++ 2d_array_on_heap.c
@@ -2,16 +2,16 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int** createArray(){
-	int a[7][4] = {	
+char** createArray(){
+	char a[7][4] = {	
 					1,2,3,4,
 					5,6,7,8
 				};
 
-	int **ptr;
-	ptr = (int **) malloc(sizeof(int *) * 7);
+	char **ptr;
+	ptr = (char **) malloc(sizeof(char *) * 7);
 	for(int i=0;i<7;i++){
-		ptr[i] = (int *) malloc(sizeof(int) * 4); 
+		ptr[i] = (char *) malloc(sizeof(char) * 4); 
 		for(int j=0;j<4;j++){
 			ptr[i][j] = a[i][j];
 		}
@@ -20,8 +20,17 @@ int** createArray(){
 	return ptr;
 
 }
+
+void freeArray(void **arr,int x){
+	for(int i=0;i<x;i++){
+		free(arr[i]);
+	}
+
+	free(arr);
+}
+
 int main(){
-	int **arr = createArray();
+	char **arr = createArray();
 
 	for(int i=0;i<7;i++){
 		for(int j=0;j<4;j++){
@@ -29,5 +38,7 @@ int main(){
 		}
 		printf("\n");
 	}
+
+	freeArray(arr, 7);
 	return 0;
 }
