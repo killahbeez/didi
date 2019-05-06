134dd78d820ee76a65565009708dd1c729af8b39
Adrian <adrian.carstea@gmail.com>
2019-05-07 02:16:19 +0300

    packed structure

--- ex6.c
+++ ex6.c
@@ -1,11 +1,19 @@
-
 #include <stdio.h>
 #include <stdint.h>
-#include "ex5.h"
-extern int a;
+#include <string.h>
+#include <stdlib.h>
+
+struct __attribute__((__packed__)){
+		char a;
+		uint32_t b;
+} didi = {1,1};
+
 int main(){
-	didi();	
-	didi();	
-	printf("%d\n",a);
+	
+	printf("%d %d\n",didi.a, didi.b);
+	printf("%d\n",sizeof(didi));
+
+	if(didi.a && didi.b) printf("ok\n");
+	if(!(!didi.a || !didi.b)) printf("ok\n");
 	return 0;
 }
