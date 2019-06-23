4361a6ac4b33dd2124444a2d8c740434fc3dbd81
Adrian <adrian.carstea@gmail.com>
2019-06-23 19:25:23 +0300

    xor reverse string

--- reverse_string.c
+++ reverse_string.c
@@ -4,11 +4,17 @@
 #include <string.h>
 
 char *reverse_str(char *str);
+char *reverse_str_XOR(char *str);
+
 int main(int argc, char *argv[]){
 	char *didi = "abcdefghijklmnoprstuvwxyz";
 	char *rev_didi = reverse_str(didi);
+	char *didi_1 = "abcdefghijklmnoprstuvwxyz";
+	char *rev_didi_1 = reverse_str_XOR(didi_1);
 	printf("Length: %d\nString: %s\nLength: %d\nReverse string: %s\n",strlen(didi),didi,strlen(rev_didi),rev_didi);
+	printf("Length: %d\nString: %s\nLength: %d\nReverse string: %s\n",strlen(didi_1),didi_1,strlen(rev_didi_1),rev_didi_1);
 	free(rev_didi);
+	free(rev_didi_1);
 	return 0;
 }
 
@@ -21,5 +27,20 @@ char *reverse_str(char *str){
 		size--;
 	}
 	*(buffer+i) = '\0';
+	return buffer;
+}
+
+char *reverse_str_XOR(char *str){
+	uint16_t size = strlen(str);
+	char *buffer = (char *) malloc(size+1);
+	memcpy(buffer,str,size+1);
+	int i=0;
+	while(i+1<size){
+		*(buffer + i) ^= *(buffer+size-1);
+		*(buffer + size -1) ^= *(buffer+i);
+		*(buffer + i) ^= *(buffer+size-1);
+		size--;
+		i++;
+	}
 	return buffer;
 }
